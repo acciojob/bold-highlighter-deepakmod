357d68ddc31d812038c54c8199e6b3c3df58cb45
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-23 18:02:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function highlight() {
-    //Write your code here
-
-
-}
-
-
-function return_normal() {
-    //Write your code here
-
-    
-}
+        let link=document.getElementsByTagName("a")[0];
+        let bold=document.getElementsByTagName("strong");
+        function highlight(){
+            for (let i = 0; i < bold.length; i++) {
+                bold[i].style.color="green";
+            }
+        }
+        function return_normal(){
+            for (let i = 0; i < bold.length; i++) {
+                bold[i].style.color="black";
+            }
+        }
